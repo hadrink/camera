f8d3f1713dc724377078fdd8509a7fb4161bda84
RomainRui <romain.rui10@gmail.com>
2017-05-22 20:50:38 +0200

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by Rplay on 22/05/2017.
+//  Copyright © 2017 Rplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
